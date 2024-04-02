2b3b0be20ee546a5dee1d5e122a711a29ce1c5b2
Bilal ABDUL <abdulbilal15@outlook.fr>
2024-04-02 17:21:53 -0400

    Fixed not importable mujoco if not installed

--- shoulder/__init__.py
+++ shoulder/__init__.py
@@ -1,3 +1,9 @@
 from .enums import MuscleCoefficientPlots, MuscleSurfacePlots
-from .models import ModelBiorbd, ModelMujoco, ControlsTypes, IntegrationMethods, MuscleParameter
+from .models import ModelBiorbd, ControlsTypes, IntegrationMethods, MuscleParameter
+
+try:
+    from .models import ModelMujoco
+except ImportError:
+    pass
+
 from .visual import Plotter
--- shoulder/models/__init__.py
+++ shoulder/models/__init__.py
@@ -1,4 +1,8 @@
 from .enums import ControlsTypes, IntegrationMethods, MuscleParameter
 from .model_abstract import ModelAbstract
 from .model_biorbd import ModelBiorbd
-from .model_mujoco import ModelMujoco
+
+try:
+    from .model_mujoco import ModelMujoco
+except ModuleNotFoundError:
+    pass
