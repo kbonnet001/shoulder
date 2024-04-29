dc7933adf86ead04aff8ab7e2340534a55f5d501
Kloé Bonnet <kbonnet001@ensc.fr>
2024-04-29 17:17:10 -0600

    début du stage, 1 cylindre, 2 points et un segment, équation de droite

--- run/muscle_geometry.py
+++ run/muscle_geometry.py
@@ -1,22 +1,61 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# import casadi 
+def findEquation(point_1,point_2):
+  # Point_1 and point_2 are two points (origin and final)
+  # point = [point_x, point_y, point_z]
+  # z=ax+by+c 
 
-# a = casadi.MX.sym("a", 3, 2)
-# b = casadi.MX.sym("b", 2, 6)
-# c = casadi.MX.sym("c", 3, 2)
+  # Calculation of the leading coefficients a and b
+  a = (point_2[2] - point_1[2]) / (point_2[0] - point_1[0])
+  b = (point_2[2] - point_1[2]) / (point_2[1] - point_1[1])
 
-# d = (c + a) @ (b * b)
+  # Calculation of the intercept c
+  c = point_1[2] - a * point_1[0] - b * point_1[1]
 
-# d_func = casadi.Function("d", [a, b, c], [d]).expand()
-# d_jaco_func = casadi.Function("d", [a, b, c], [casadi.jacobian(d, a)])
-# print(d_func(np.ones((3, 2)), np.ones((2, 6)), np.ones((3, 2))))
-# print(d_jaco_func(np.ones((3, 2)), np.ones((2, 6)), np.ones((3, 2))))
+  return a, b, c
+
+def data_create_Cylinder(center_x, center_y, radius, height_z) :
+  z = np.linspace(0, height_z, 50)
+  theta = np.linspace(0, 2*np.pi, 50)
+  theta_grid, z_grid=np.meshgrid(theta, z)
+  x_grid = radius*np.cos(theta_grid) + center_x
+  y_grid = radius*np.sin(theta_grid) + center_y
+
+  return x_grid, y_grid, z_grid
 
 def main():
-    origin_point = np.array([0, 0, 0])
-    insertion_point = np.array([0, 0, 1])
+  
+   # Set figure
+   fig = plt.figure()
+   ax = fig.add_subplot(111, projection='3d')
+  
+   # Points
+   origin_point = [0.1,0.5,0.5]
+   final_point=[0.5,0.1,0.5]
 
+   # Points for cylinder
+   center_x=0.2
+   center_y=0.2
+   radius=0.05 # for cylinder "circle"
+   height_z=0,1 
+
+   Xc,Yc,Zc = data_create_Cylinder(center_x, center_y, radius, height_z)
+   ax.plot_surface(Xc, Yc, Zc, alpha=0.5)
+  
+   # Set graph
+   ax.set_xlabel("X")
+   ax.set_ylabel("Y")
+   ax.set_zlabel("Z")
+   ax.grid(True)
+   #plt.plot(origin_point, insertion_point)
+   ax.scatter(*origin_point, color='blue', label="origin")
+   ax.scatter(*final_point, color='red', label="final")
+
+   ax.plot(*zip(origin_point, final_point), color='green')
+
+   plt.show()
 
 if __name__ == "__main__":
-    main()
+   main()
