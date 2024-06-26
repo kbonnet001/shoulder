83e92ee576e3a80f533d50f0cfa22e0c7fd5580a
Kloe <kbonnet001@ensc.fr>
2024-06-26 15:26:10 -0300

    essai de test avec artcan2 mais pas convaincant...

--- wrapping/paspropre.py
+++ wrapping/paspropre.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import copy
 
 def angle_between_points(p1, p2):
     # Calcul du produit scalaire
@@ -65,23 +66,65 @@ def determine_if_needed_change_side(point, point_ref_q_initial) :
     else : 
         return False
 
-def determine_if_needed_change_side_2(point, point_ref_q_initial) :
-    angle = angle_between_points(point_ref_q_initial[:2], [1.0, 0.0] ) 
+# def determine_if_needed_change_side_2(point, point_ref_q_initial) :
+#     angle = angle_between_points(point_ref_q_initial[:2], [1.0, 0.0] ) 
+#     angle_rad = math.radians(angle)
+#     R = roto_trans_matrix(angle_rad)
+#     # R = np.array([[ 0.96266626 ,-0.27069111  ,0.      ,    0.        ],
+#     #               [ 0.27069111 , 0.96266626 , 0.      ,    0.        ],
+#     #               [ 0.         , 0.         , 1.      ,    0.        ],
+#     #               [ 0.         , 0.         , 0.       ,   1.        ]])
+#     point = apply_transformation(point, R)
+    
+#     print("point transforme = ", point)
+
+#     # if point[1] < 0 and point: 
+#     #   return True
+#     # else :
+#     #   return False
+#     if point[0] > 0  :
+#         return True
+#     else : 
+#         return False
+
+
+def point_to_initial(points, point_ref) :
+    # pas tres tres english
+    # to do a rotation, we want point ref at (1, 0, z)
+    # ans we also want other point (points) moove in the same way
+    #points [] list of point dont point ref
+    angle = angle_between_points(point_ref[:2], [1.0, 0.0] ) 
     angle_rad = math.radians(angle)
     R = roto_trans_matrix(angle_rad)
-    # R = np.array([[ 0.96266626 ,-0.27069111  ,0.      ,    0.        ],
-    #               [ 0.27069111 , 0.96266626 , 0.      ,    0.        ],
-    #               [ 0.         , 0.         , 1.      ,    0.        ],
-    #               [ 0.         , 0.         , 0.       ,   1.        ]])
-    point = apply_transformation(point, R)
-    
-    print("point transforme = ", point)
 
-    # if point[1] < 0 and point: 
-    #   return True
-    # else :
-    #   return False
-    if point[0] > 0  :
-        return True
-    else : 
-        return False
+    transformed_points = []
+    
+    for point in points : 
+        point_rot = apply_transformation(point, R)
+        transformed_points.append(point_rot)
+        print("point transforme = ", point_rot)
+         
+    return transformed_points
+    
+   
+def arctan2_corrected(v1, v2) : 
+    v1_rot, v2_rot = point_to_initial([v1, v2], v1)
+    arctan2 = np.arctan2(v2_rot[1], v2_rot[0])
+    print("arctan2 = ", arctan2)
+    if arctan2 < 0 : 
+        # arctan2 = arctan2 + 360 * np.pi/180
+        v1_rot, v2_rot = point_to_initial([v1, v2], v2)
+        arctan2 = np.arctan2(v1_rot[1], v1_rot[0])
+        print("arctan2 + 180 = ", arctan2)
+    return arctan2
+
+def arctan2_corrected(v1, v2) : 
+    v1_rot, v2_rot = point_to_initial([v1, v2], v1)
+    arctan2 = np.arctan2(v2_rot[1], v2_rot[0])
+    print("arctan2 = ", arctan2)
+    if arctan2 < 0 : 
+        arctan2 = arctan2 + 360 * np.pi/180
+        # v1_rot, v2_rot = point_to_initial([v1, v2], v2)
+        # arctan2 = np.arctan2(v1_rot[1], v1_rot[0])
+        print("arctan2 + 180 = ", arctan2)
+    return arctan2
