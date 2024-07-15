0ac4fa04c4953a726286513eb07cd93cf4b92807
Kloe <kbonnet001@ensc.fr>
2024-07-15 11:58:01 -0300

    commentaires et rangement code

--- run/muscle_geometry.py
+++ run/muscle_geometry.py
@@ -1,28 +1,19 @@
 import numpy as np
-import pandas as pd
-import copy
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from scipy.linalg import norm
 from wrapping.plot_cylinder import *
 from wrapping.algorithm import*
 from wrapping.Cylinder import Cylinder
 from neural_networks.discontinuities import *
 import torch.nn as nn
-import torch
 from neural_networks.Loss import *
 from pyorerun import LiveModelAnimation
 
-from sklearn.model_selection import train_test_split
-# from neural_networks.data_preparation import print_informations_environment
-# from neural_networks.main_trainning import main_superised_learning
-
 from neural_networks.data_generation import *
-from neural_networks.main_trainning import *
 from neural_networks.ModelHyperparameters import ModelHyperparameters
 from neural_networks.data_generation_ddl import data_for_learning_ddl, plot_one_q_variation, plot_all_q_variation, data_for_learning_without_discontinuites_ddl, data_generation_muscles
 from neural_networks.k_cross_validation import cross_validation, try_best_hyperparams_cross_validation
 from neural_networks.functions_data_generation import compute_q_ranges
+from wrapping.tensor import plot_tensor
 
 #################### 
 # Code des tests
@@ -41,24 +32,6 @@ from wrapping.wrapping_tests.step_1_test import Step_1_test
 
 model = biorbd.Model("models/Wu_DeGroote.bioMod")
 
-# def compute_q_ranges_segment(model, segment_selected) : 
-#     # segment_names
-#     # ['thorax', 'spine', 'clavicle_effector_right', 'clavicle_right', 'scapula_effector_right', 'scapula_right', 
-#     # 'humerus_right', 'ulna_effector_right', 'ulna_right', 'radius_effector_right', 'radius_right', 'hand_right']
-    
-#     segment_names = [model.segment(i).name().to_string() for i in range(model.nbSegment())]
-#     humerus_index = segment_names.index(segment_selected) 
-
-#     # humerus_dof_names = [model.segment(humerus_index).nameDof(i).to_string() for i in 
-#     #                     range(model.segment(humerus_index).nbQ())]
-    
-#     q_ranges = [[ranges.min(), ranges.max()] for ranges in model.segment(humerus_index).QRanges()]
-#     return q_ranges
-
-
-# q_ranges = compute_q_ranges_segment(model, "humerus_right")
-# q_ranges.append([0.05,  2.356194490192345])
-
 q_ranges, _ = compute_q_ranges(model)
 
 
@@ -69,45 +42,9 @@ q_ranges, _ = compute_q_ranges(model)
 C_T_PECM2_1 = np.array([0.0183539873, -0.0762563082, 0.0774936934])
 C_T_PECM2_2 = np.array([0.0171218365, -0.0120059285, 0.0748758588])
 
-# Datas for cylinder's humerus right (muscle PECM1, PECM2 and PCM3)
-# C_H_PECM2_1 = np.array([-0.0468137093, -0.069205313, 0.1748923225]) # 0.02 5000_2 
-# C_H_PECM2_2 = np.array([-0.0276992818, 0.0056711748, 0.1704452973])
-
-# C_H_PECM2_1 = np.array([-0.0549820662, -0.0634122725, 0.1813890163])
-# C_H_PECM2_2 = np.array([-0.0398385092, -0.00409078, 0.1778658253]) # 0.0285
-
-# C_H_PECM2_1 = np.array([-0.0414664438, -0.0640982989, 0.175861232])
-# C_H_PECM2_2 = np.array([-0.0278003171, -0.0105643089, 0.1726817693]) #0.016
-
-# C_H_PECM2_1 = np.array([-0.0418935136, -0.0638634585, 0.1779761963])
-# C_H_PECM2_2 = np.array([-0.028227486, -0.0103298566, 0.1747967566]) # r = 0.016 5000_6 avec q_ranges_PECM2
-
-#----------------------------------------
-# re test demade de descendre le cylindre
-# C_H_PECM2_1 = np.array([-0.0425358482, -0.0644412567, 0.1813890163])
-# C_H_PECM2_2 = np.array([-0.0284186112, -0.0091401431, 0.1781046015]) # r = 0.016 
-
-# C_H_PECM2_1 = np.array([-0.0425358482, -0.0644412567, 0.1830806943])
-# C_H_PECM2_2 = np.array([-0.028370612, -0.008952117, 0.1797851123]) # r = 0.016 plus de probleme pour q fixe midle range et q0 varie
-
-# C_H_PECM2_1 = np.array([-0.0424215591, -0.0643620752, 0.1829936853])
-# C_H_PECM2_2 = np.array([-0.0283105583, -0.0090853905, 0.1797107213]) # r = 0.016 5000_ 3 et 4
-
-# C_H_PECM2_1 = np.array([-0.0409602858, -0.0641361296, 0.1736150806])
-# C_H_PECM2_2 = np.array([-0.0274015472, -0.0110228084, 0.170460602]) # r = 0.016 descendre
-
-# re essai des cadrans
-# C_H_PECM2_1 = np.array([-0.03848864,-0.05134674,0.1740978])
-# C_H_PECM2_2 = np.array([-0.0137964 ,  0.04537976,  0.16835304]) # r = 0.016 fait à partir de insertion en ref segment humerus local
-
 C_H_PECM2_1 = np.array([-0.0504468139, -0.0612220954, 0.1875298764])
 C_H_PECM2_2 = np.array([-0.0367284615, -0.0074835226, 0.1843382632]) #le mieux avec 0.025 0.0243 vrai 0.0255913399
-
-# Presque la fin j'espere ahhhh
-# C_H_PECM2_1 = np.array([-0.0439209708, -0.0622294269, 0.1772302577])
-# C_H_PECM2_2 = np.array([-0.030809488, -0.0108681304, 0.1741798345]) # pas bien, ne pas faire des cylinre avec r trop petit !
-
-#################################################################################################
+# -----------------------------------------------------------------
 cylinder_T_PECM2 = Cylinder.from_points(0.025, -1, C_T_PECM2_2, C_T_PECM2_1, False, "thorax", "PECM2")
 cylinder_H_PECM2 = Cylinder.from_points(0.0255913399, 1, C_H_PECM2_2, C_H_PECM2_1, True, "humerus_right", "PECM2")
 
@@ -121,7 +58,7 @@ cylinder_T_PECM3 = Cylinder.from_points(0.025, -1, C_T_PECM3_2, C_T_PECM3_1, Fal
 cylinder_H_PECM3 = Cylinder.from_points(0.0202946443, 1, C_H_PECM3_2, C_H_PECM3_1, True, "humerus_right", "PECM3")
 
 # cylinder_H_PECM2.rotate_around_axis(-45)
-
+# -----------------------------------------------------------------
 cylinders_PECM2=[cylinder_T_PECM2, cylinder_H_PECM2]
 cylinders_PECM3=[cylinder_T_PECM3, cylinder_H_PECM3]
 
@@ -129,18 +66,15 @@ cylinders = [cylinders_PECM2, cylinders_PECM3]
 
 muscles_selected = ["PECM2", "PECM3"]
 segments_selected = ["thorax", "humerus_right"] # pour le moment, on change rien
+# -----------------------------------------------------------------
 
 # test_limit_data_for_learning(muscles_selected[0],cylinders_PECM2, model, q_ranges, True, False) 
 
 # data_for_learning (muscles_selected[0],cylinders_PECM2, model, q_ranges, 5000, "df_PECM2_datas_without_error_partfdsadaf_5000.xlsx", False, False) 
 
 # data_for_learning_ddl (muscles_selected[0], cylinders_PECM2, model, 10, "rgtrsfdd.xlsx", data_without_error = True, plot=False, plot_cadran = False)
-   
-# ----------------------
-# train_model_supervised_learning("df_PECM2_datas_5000_more.xlsx")
-# print_informations_environment()
-# main_superised_learning("df_PECM2_datas_25000.xlsx", True, "model_weights.pth")
-# ----------------------
+
+# -----------------------------------------------------------------
 
 # q_fixed = np.array([(ranges[0] + ranges[-1]) / 2  for ranges in q_ranges])
 q_fixed = np.array([0.0 for k in range (10)])
@@ -158,7 +92,7 @@ q_fixed = np.array([0.0 for k in range (10)])
 #----------------
 # data_for_learning_without_discontinuites_ddl(muscles_selected[0], cylinders[0], model, 5010, "data_generation_data_more_ddl_6/PECM2", num_points = 100, plot_cylinder_3D=False, plot_discontinuities = False, plot_cadran = False, plot_graph=True)
 
-# data_generation_muscles(muscles_selected, cylinders, model, 10000, "datas", num_points = 20, plot_cylinder_3D=False, plot_discontinuities = False, plot_cadran = False, plot_graph=False)
+# data_generation_muscles(muscles_selected, cylinders, model, 10000, "datas_poubelle", num_points = 20, plot_cylinder_3D=False, plot_discontinuities = False, plot_cadran = False, plot_graph=False)
 
 
 # --------------------
@@ -166,7 +100,7 @@ q_fixed = np.array([0.0 for k in range (10)])
 #                 "df_PECM3_datas_without_error_part_5000.xlsx", num_points = 50, plot_discontinuities = False, 
 #                 plot=False, plot_cadran = False)
    
-   
+# -----------------------------------------------------------------
 
 P = np.array([-3.78564,-2.53658,0])
 S = np.array([7.0297,1.44896,1.21311])
@@ -186,7 +120,7 @@ cylinder_2 = Cylinder.from_points(1,-1, c21, c22)
 # v1o, v2o, obstacle_tangent_point_inactive, segment_length = single_cylinder_obstacle_set_algorithm(P, S, cylinder_1)
 # plot_one_cylinder_obstacle(P, S, cylinder_1, v1o, v2o,obstacle_tangent_point_inactive)
 
-
+# -----------------------------------------------------------------
 # Show biorbd
 # q = np.zeros((model.nbQ(), ))
 # b = bioviz.Viz(loaded_model=model)
@@ -195,18 +129,12 @@ cylinder_2 = Cylinder.from_points(1,-1, c21, c22)
 
 # exit(0)
 
-# pour voir pyorerun
+# # pour voir pyorerun
 # model_path = "/home/lim/Documents/kloe/shoulder/run/models/Wu_DeGroote.bioMod"
 # animation = LiveModelAnimation(model_path, with_q_charts=True)
 # animation.rerun()
 
-
-#################
-p0 = np.array([1.0, 1.0])
-p1 = np.array([2.0, 2.0])
-p2 = np.array([3.0, 3.0])
-
-# find_discontinute(p0, p1, p2)
+# -----------------------------------------------------------------
 
 # data_loaders = prepare_data_from_folder(32, "datas", plot=False)
 # print("")
@@ -229,7 +157,6 @@ p2 = np.array([3.0, 3.0])
 # ]
 # p_dropout = [0.2, 0.5]
 # use_batch_norm = True
-
 
 model_name="essai"
 batch_size=32
@@ -255,10 +182,10 @@ Hyperparameter_essai1 = ModelHyperparameters(model_name, batch_size, n_layers, n
                                              use_batch_norm)
 
 # one model per muscle !
-main_superised_learning(Hyperparameter_essai1, q_ranges, folder_name="datas", muscle_name = "PECM2", retrain=False, 
-                        file_path=Hyperparameter_essai1.model_name,plot_preparation=False, plot=True, save=True) 
-main_superised_learning(Hyperparameter_essai1, q_ranges, folder_name="datas", muscle_name = "PECM3", retrain=False, 
-                        file_path=Hyperparameter_essai1.model_name,plot_preparation=True, plot=True, save=True) 
+# main_superised_learning(Hyperparameter_essai1, q_ranges, folder_name="datas", muscle_name = "PECM2", retrain=False, 
+#                         file_path=Hyperparameter_essai1.model_name,plot_preparation=False, plot=True, save=True) 
+# main_superised_learning(Hyperparameter_essai1, q_ranges, folder_name="datas", muscle_name = "PECM3", retrain=False, 
+#                         file_path=Hyperparameter_essai1.model_name,plot_preparation=True, plot=True, save=True) 
 
 # list_simulation, best_hyperparameters_loss, best_hyperparameters_acc = find_best_hyperparameters(Hyperparameter_essai1, folder_name)
 # all_cross_val_test = try_best_hyperparams_cross_validation(folder_name, list_simulation, num_try_cross_validation , num_folds)
@@ -266,6 +193,29 @@ main_superised_learning(Hyperparameter_essai1, q_ranges, folder_name="datas", mu
 print("")
 
 # # cross_validation("datas/error_part", Hyperparameter_essai1, num_folds)
+
+
+# -----------------------------------------------------------------
+
+# ------------
+q_initial = np.array([0.0 for k in range (8)])
+# essai_dlmt_dq(model, q, 0, delta_qi = 1e-4)
+
+# --------------------------
+# test
+
+# NE FONCTIONNE QUE POUR PECM2 !
+muscle_index = 0 #(PECM2)
+muscle_selected = "PECM2"
+cylinders_PECM2=[cylinder_T_PECM2, cylinder_H_PECM2]
+q= np.array([(ranges[0] + ranges[-1]) / 2  for ranges in q_ranges])
+
+# compute_lmt(model, q, cylinders_PECM2, muscle_index, plot=False, plot_cadran = False)
+
+plot_tensor(model, q_initial, cylinders_PECM2, muscle_selected, "without_wrappingkjhgkhg",100)
+
+
+
 
 
 
--- wrapping/tensor.py
+++ wrapping/tensor.py
@@ -1,59 +1,138 @@
 from scipy.linalg import norm
 from neural_networks.functions_data_generation import update_points_position
 import copy
-import casadi as ca
+import matplotlib.pyplot as plt
+from neural_networks.functions_data_generation import *
+import numpy as np
 
+# -------------------------------------
 def dev_partielle_lmt_qi_points_without_wrapping (lmt1, lmt2, delta_qi) : 
-    #p1 p2 point 3D
-    # q q len qranges
-    # qi index i
-    # delta qi tre petite variation de q, attention vect aue des 0 et juste le qi aue l'on vut epsilon
     
-    return (lmt1 - lmt2) / (2*delta_qi)
+   """ Compute the partial derivative of lmt as a function of qi
+   
+   INPUTS : 
+   - lmt1 : float, lmt1 = lmt(q + vect_delta_qi)
+   - lmt2 : float, lmt2 = lmt(q - vect_delta_qi)
+   - delta_qi : float, small variation of  q (for example, 1e-3, 1e-4, ...)
+   
+   OUTPUT :
+   - dlmt_dqi : float, partial derivative of lmt as a function of qi   """
+   
+   return (lmt1 - lmt2) / (2*delta_qi)
 
-def essai_dlmt_dq(model, q, muscle_index, delta_qi = 1e-4) :
+def compute_dlmt_dq(model, q, cylinders, muscle_index, delta_qi=1e-8) : 
     
-    dlmt_dq = []
+   """ Compute the partial derivative of lmt as a function of q 
+   
+   NOTE : We can't use directly "compute_segment_length" because it just return le length in regard of wrapping
+   But in this case, if there is no wrapping, we compute dlmt_dq thank via points. 
+   None alternatives exist for a situation "wrapping + via points" 
+   so the resultats with biorbd could be very different (discontnuities)
+   
+   INPUTS : 
+   - model : biorbd.Model
+   - q : array qi 
+   - cylinders : list of Cylinder, cylinders associate with muscle selected (=[] if no cylinders)
+   - muscle_index : index of muscle selected
+   - delta_qi : (default = 1e-8) float, a small variation for all qi 
+   
+   OUTPUS : 
+   - dlmt_dq : array 1*len(q) float, the partial derivative of lmt as a function of q """
+   
+   dlmt_dq = []
+   
+   for i in range(len(q)) : 
+        
+      # Create vect delta_qi
+      vect_delta_qi = [0 for k in range(len(q))]
+      vect_delta_qi[i] = delta_qi
+      
+      if len(cylinders) != 0 : 
+        # if no cylinders, use via points to compute le length
+        origin_point_pos, insertion_point_pos = update_points_position(model, [0, -1], muscle_index, q + vect_delta_qi)
+        origin_point_neg, insertion_point_neg = update_points_position(model, [0, -1], muscle_index, q - vect_delta_qi)
+          
+        lmt1, _ = compute_segment_length(model, cylinders, q, origin_point_pos, insertion_point_pos, False, False)
+        lmt2, _ = compute_segment_length(model, cylinders, q, origin_point_neg, insertion_point_neg, False, False)
+      else : 
+        mus = model.muscle(muscle_index) 
+        p_pos = list(update_points_position(model, [n for n in range(len(mus.musclesPointsInGlobal(model, q)))], muscle_index, q + vect_delta_qi))
+        p_neg = list(update_points_position(model, [n for n in range(len(mus.musclesPointsInGlobal(model, q)))], muscle_index, q - vect_delta_qi))
+        
+        lmt1 = sum(norm(np.array(p_pos[n]) - np.array(p_pos[n+1])) for n in range (len(p_pos)-1))
+        lmt2 = sum(norm(np.array(p_neg[n]) - np.array(p_neg[n+1])) for n in range (len(p_neg)-1))
+   
+      dlmt_dqi = dev_partielle_lmt_qi_points_without_wrapping(lmt1, lmt2, delta_qi)
+      
+      dlmt_dq.append(copy.deepcopy(dlmt_dqi))
+      
+   print("dlmt_dq = ", dlmt_dq)
+   
+   return dlmt_dq
+ 
+ 
+def plot_tensor(model, q_fixed, cylinders, muscle_selected, filename, num_points = 100) : 
     
-    for i in range(len(q)) : 
-        
-        # Create vect delta_qi
-        q_delta_qi = [0 for k in range(len(q))]
-        q_delta_qi[i] = delta_qi
-        
-        origin_point_pos, insertion_point_pos = update_points_position(model, [0, -1], muscle_index, q + q_delta_qi)
-        origin_point_neg, insertion_point_neg = update_points_position(model, [0, -1], muscle_index, q - q_delta_qi)
-        
-        lmt1 = norm(origin_point_pos - insertion_point_pos)
-        lmt2 = norm(origin_point_neg - insertion_point_neg)
-        
-        dlmt_dqi = dev_partielle_lmt_qi_points_without_wrapping(lmt1, lmt2, delta_qi)
-        
-        dlmt_dq.append(copy.deepcopy(dlmt_dqi))
-    
-    return dlmt_dq
+   """ Plot and save a comparaison of variation of dlmt_dq (with wrappings) and dlmt_dq_biorbd (with via points) 
+   
+   INPUTS : 
+   - model : biorbd.Model
+   - q_fixed : array q value choosen by user
+   - cylinders : list of Cylinder, cylinders associate with muscle selected (=[] if no cylinders)
+   - muscle_selected : string, muscle selected
+   - filename : string, filename/file_path to save the plot
+   - num_points : (default = 100) int, number of points for plot
+   
+   OUTPUT : 
+   - None, save plot"""
+   
+   q_ranges, q_ranges_names_with_dofs = compute_q_ranges(model)
+   
+   muscle_index= initialisation_generation(model, q_ranges, muscle_selected, cylinders)
+   delta_qi = 1e-8
+   
+   fig, axs = plt.subplots(3, (len(q_ranges)+1)//3, figsize=(15, 10))
+   
+   for q_index in range (len(q_ranges)) : 
+      q = copy.deepcopy(q_fixed)
+      qs = []
+      dlmt_dqis= []
+      dlmt_dq_biorbds = []
+      
+      for k in range (num_points+1) : 
+         print("q_index = ", q_index, " ; k = ", k)
 
+         qi = k * ((q_ranges[q_index][1] - q_ranges[q_index][0]) / num_points) + q_ranges[q_index][0]
+         q[q_index] = qi
+         
+         model.updateMuscles(q) 
+         model.UpdateKinematicsCustom(q)
+         dlmt_dq_biorbd = model.musclesLengthJacobian().to_array()
+         
+         dlmt_dq = compute_dlmt_dq(model, q, cylinders, muscle_index, delta_qi)
+         
+         qs.append(qi)
+         dlmt_dqis.append(dlmt_dq)
+         dlmt_dq_biorbds.append(dlmt_dq_biorbd[muscle_index])
+         
+         
+      row = q_index // ((len(q_ranges) + 1) // 3)
+      col = q_index % ((len(q_ranges) + 1) // 3)
 
-# ------------CASADI-------------------
-
-# # Déclaration des variables de décision
-# p1 = ca.SX.sym('p1', 3)
-# p2 = ca.SX.sym('p2', 3)
-# r = ca.SX.sym('r')  # /!\ radius * side cylindre
-
-# # Calcul de la première partie de la fonction
-# distance_xy = (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
-# cos_theta = 1.0 - distance_xy / (2 * r**2)
-# theta = ca.acos(cos_theta)
-# f_part1 = r * theta
-
-# # Calcul de la deuxième partie de la fonction
-# f_part2 = (p2[2] - p1[2])**2
-
-# # Somme des parties et racine carrée
-# f = ca.sqrt(f_part1 + f_part2)
-
-# print(f)
-        
-    
-    
+      for i in range(len(dlmt_dq)) :
+        if i == 0:
+            axs[row, col].plot(qs, [dlmt_dqi_biorbd[i] for dlmt_dqi_biorbd in dlmt_dq_biorbds], marker='^', linestyle='--', color = "silver", markersize=1, label=f"dlmt_dq_biorbd")
+        else:
+            axs[row, col].plot(qs, [dlmt_dqi_biorbd[i] for dlmt_dqi_biorbd in dlmt_dq_biorbds], marker='^', linestyle='--', color = "silver", markersize=1)
+            
+            axs[row, col].plot(qs, [dlmt_dqi[i] for dlmt_dqi in dlmt_dqis], marker='o', linestyle='-', markersize=1, label=f"dlmt_dq{i}")
+      axs[row, col].set_xlabel(f'q{q_index} Variation',fontsize='smaller')
+      axs[row, col].set_ylabel(f'dlmt_dq{q_index}',fontsize='smaller')
+      axs[row, col].set_title(f'{q_ranges_names_with_dofs[q_index]}',fontsize='smaller')
+      axs[row, col].legend()
+   
+   fig.suptitle(f'{filename} dlmt_dq\nq_fixed = {q_fixed}', fontweight='bold')
+   plt.tight_layout()  
+   plt.savefig(f"{filename}_dlmt_dq.png")
+   plt.show()
+      
